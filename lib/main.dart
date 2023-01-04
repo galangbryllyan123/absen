87baa13095f9802d246c3289b6aadd90c33f281f
Galang Bryllyan Wicaksono <48407794+galangbryllyan123@users.noreply.github.com>
2023-01-05 01:51:46 +0700

    senokidal

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:go_absensi/app/controllers/page_index_controller.dart';
+import 'package:go_absensi/app/controllers/presence_controller.dart';
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  Get.put(PresenceController(), permanent: true);
+  Get.put(PageIndexController(), permanent: true);
+
+  runApp(
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        }
+        return GetMaterialApp(
+          title: "Application",
+          debugShowCheckedModeBanner: false,
+          initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+          getPages: AppPages.routes,
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            fontFamily: 'inter',
+          ),
+        );
+      },
+    ),
+  );
+}
